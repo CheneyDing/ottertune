490bf5be677f3890d19ed4c4528de02918ab527a
YiqinXiong <u201714501@hust.edu.cn>
2021-07-19 18:12:17 +0800

    server端添加TiDB的相关适配(2)

---
+++ server/website/script/fixture_generators/knob_settings/tidb_5.0/create_knob_settings.py
@@ -0,0 +1,9 @@
+#
+# Created on July 19, 2021
+#
+# @author: Yiqin Xiong
+#
+
+# TODO: 仿照oracle和postgres，从导出的knobs定义文件，转换为标准的metrics的json文件，拷贝到fixture目录中
+# NOTE：sap hana的添加中没有这一步骤（原因未知）
+# NOTE：流程大概是 1.导出的metrics定义文件通过本文件得到fixture目录下标准的metrics的json文件 2.
